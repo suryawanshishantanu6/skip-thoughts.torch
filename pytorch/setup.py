5fa4ad4e3424cc5bba6e0f6ee1fa41337280de3a
suryawanshishantanu6 <suryawanshishantanu6@gmail.com>
2018-09-11 22:13:24 +0530

    Update setup.py

--- pytorch/setup.py
+++ pytorch/setup.py
@@ -21,7 +21,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 # Fields marked as "Optional" may be commented out.
 
 # https://stackoverflow.com/questions/458550/standard-way-to-embed-version-into-python-package/16084844#16084844
-exec(open('skipthoughts/version.py').read())
+#exec(open('skipthoughts/version.py').read())
 setup(
     # This is the name of your project. The first time you publish this
     # package, this name will be registered for you. It will determine how
@@ -42,7 +42,7 @@ setup(
     # For a discussion on single-sourcing the version across setup.py and the
     # project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version=__version__,  # Required
+    version=__version__ = '0.0.0',  # Required
 
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
